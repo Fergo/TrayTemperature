486c341a71c05db244e81ced3e24a8003befe9f3
Fergo <fergobirck@live.com>
2021-02-22 16:53:31 -0300

    Added statistics (avg, min, max) to the tooltip and log

--- TrayTemperature/Program.cs
+++ TrayTemperature/Program.cs
@@ -7,20 +7,28 @@ using System.Drawing;
 using System.IO;
 
 using OpenHardwareMonitor.Hardware;
+using System.Text;
+using System.Reflection;
 
 namespace TrayTemperature {
 	static class Program {
 
 		static Computer computer = new Computer() { CPUEnabled = true, GPUEnabled = true };
-		static int CPU = 0, GPU = 0;
+		static int CPU = 0, GPU = 0, CPUMax = 0, GPUMax = 0, CPUMin = 99999, GPUMin = 99999;
+		static ulong CPUAcc = 0, GPUAcc = 0, regCount = 0;
 		static bool isLogging = false;
 		static Timer tmr;
 		static NotifyIcon ni;
 		static ContextMenu contextMenu;
 		static StreamWriter sw;
 
+
+
 		[STAThread]
 		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
 			Properties.Settings.Default.Upgrade();
 
 			//Inititalize OpenHardwareMonitorLib
@@ -30,8 +38,6 @@ namespace TrayTemperature {
 			tmr = new Timer {
 				Interval = Properties.Settings.Default.Refresh * 1000,
 				Enabled = true
-
-			
 			};
 
 			tmr.Tick += tmr_tick;
@@ -70,6 +76,10 @@ namespace TrayTemperature {
 				new MenuItem {
 					Name = "menLog",
 					Text = "Log"
+				},
+				new MenuItem {
+					Name = "menReset",
+					Text = "Reset statistics"
 				},
 				new MenuItem {
 					Text = "-",
@@ -162,24 +172,51 @@ namespace TrayTemperature {
 				case "menExit":
 					Application.Exit();
 					break;
+				case "menReset":
+					CPUMax = 0; GPUMax = 0; CPUAcc = 0; GPUAcc = 0; regCount = 0; CPUMin = 99999; GPUMin = 99999;
+					break;
 				case "menLog":
 					if (!isLogging) {
-						sw = new StreamWriter(string.Format("{0:yyyy-MM-dd_hh-mm-ss}.log", DateTime.Now));
+						if (MessageBox.Show("Starting a log will reset the current average, minimum and maximum temperatures. Proceed?", "Log start", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+							return;
+
+						sw = new StreamWriter(string.Format("temp.log", DateTime.Now), false);
 						sw.WriteLine("DateTime,CPU Temperature,GPU Temperature");
+
+						CPUMax = 0; GPUMax = 0;	CPUAcc = 0; GPUAcc = 0; regCount = 0; CPUMin = 99999; GPUMin = 99999;
 
 						contextMenu.MenuItems.Find("menLog", false).First().Checked = true;
 						isLogging = true;
-					} else {
-						MessageBox.Show("Log saved to:\r\n\r\n" + ((FileStream)(sw.BaseStream)).Name, "Log saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+						contextMenu.MenuItems.Find("menCel", false).First().Enabled = false;
+						contextMenu.MenuItems.Find("menFah", false).First().Enabled = false;
+					} else {
 						sw.Close();
 						sw = null;
 
+						//Create the summary table
+						StringBuilder sb = new StringBuilder();
+						sb.AppendLine("Hardware,Average,Minimum,Maximum");
+						sb.AppendLine(string.Format("CPU,{0:F2},{1},{2}", (float)CPUAcc / regCount, CPUMin, CPUMax));
+						sb.AppendLine(string.Format("GPU,{0:F2},{1},{2}", (float)GPUAcc / regCount, GPUMin, GPUMax));
+						sb.AppendLine("");
+
+						//Append the summary table with the temp timeseries
+						string fileName = string.Format("{0:yyyy-MM-dd_hh-mm-ss}.log", DateTime.Now);
+						File.WriteAllText(fileName, sb.ToString() + File.ReadAllText("temp.log"));
+						File.Delete("temp.log");
+						MessageBox.Show("Log saved to:\r\n\r\n" + Path.Combine(Application.ExecutablePath, fileName), "Log saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
 						contextMenu.MenuItems.Find("menLog", false).First().Checked = false;
 						isLogging = false;
+
+						contextMenu.MenuItems.Find("menCel", false).First().Enabled = true;
+						contextMenu.MenuItems.Find("menFah", false).First().Enabled = true;
 					}
 					break;
 			}
+
+			tmr_tick(null, null);
 		}
 
 		//Updates the temperatures
@@ -220,13 +257,55 @@ namespace TrayTemperature {
 			int convertedGPU = Convert.ToInt32(Properties.Settings.Default.Celsius ? GPU : GPU * 1.8 + 32);
 			string tempUnit = Properties.Settings.Default.Celsius ? "°C" : "°F";
 
+			CPUAcc += (ulong)convertedCPU;
+			GPUAcc += (ulong)convertedGPU;
+			regCount++;
+
+			if (CPU > CPUMax)
+				CPUMax = CPU;
+
+			if (CPU < CPUMin)
+				CPUMin = CPU;
+
+			if (GPU > GPUMax)
+				GPUMax = GPU;
+
+			if (GPU < GPUMin)
+				GPUMin = GPU;
+
 			//Appends a new line to the current log file (CSV format)
-			if (isLogging && sw != null)
+			if (isLogging && sw != null) {
 				sw.WriteLine(DateTime.Now.ToString() + "," + convertedCPU + "," + convertedGPU);
-			
+			}
+
 			//Updates the icon and tooltip
-			ni.Text = $"CPU Temperature: {convertedCPU}{tempUnit}\r\nGPU Temperature: {convertedGPU}{tempUnit}";
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("CPU");
+			sb.AppendLine($"  Cur: {convertedCPU}{tempUnit}");
+			sb.AppendLine($"  Avg: {(float)CPUAcc / regCount:F2}{tempUnit}");
+			sb.AppendLine($"  Min: {CPUMin}{tempUnit}");
+			sb.AppendLine($"  Max: {CPUMax}{tempUnit}");
+			sb.AppendLine("GPU");
+			sb.AppendLine($"  Cur: {convertedGPU}{tempUnit}");
+			sb.AppendLine($"  Avg: {(float)GPUAcc / regCount:F2}{tempUnit}");
+			sb.AppendLine($"  Min: {GPUMin}{tempUnit}");
+			sb.AppendLine($"  Max: {GPUMax}{tempUnit}");
+
+			Fixes.SetNotifyIconText(ni, sb.ToString());
+
 			ni.Icon = DynamicIcon.CreateIcon(convertedCPU.ToString() + tempUnit, cpuColor, convertedGPU.ToString() + tempUnit, gpuColor);
+		}
+	}
+
+	//Little hack to bypass the 63 char limit of the tooltip (still limited to the 128 chars of the original win32)
+	public class Fixes {
+		public static void SetNotifyIconText(NotifyIcon ni, string text) {
+			if (text.Length >= 128) throw new ArgumentOutOfRangeException("Text limited to 127 characters");
+			Type t = typeof(NotifyIcon);
+			BindingFlags hidden = BindingFlags.NonPublic | BindingFlags.Instance;
+			t.GetField("text", hidden).SetValue(ni, text);
+			if ((bool)t.GetField("added", hidden).GetValue(ni))
+				t.GetMethod("UpdateIcon", hidden).Invoke(ni, new object[] { true });
 		}
 	}
 
